28eab75e9acacb5d154c81f491c9a9e6af13420d
Ismail <ismail@sam-media.com>
2016-06-03 10:57:16 +0800

    Add computer assisted instrucion lib

---
+++ computer-assisted-instruction/Analysis/Desktop/Basic.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace computer_assisted_instruction.Analysis.Desktop
+{
+    public class Basic
+    {
+        private static Color backgroundColor = Color.FromArgb(67, 149, 209);
+        const int WM_COMMAND = 0x111;
+        const int MIN_ALL = 419;
+        const int MIN_ALL_UNDO = 416;
+        private const int SW_NORMAL = 1;
+        private const int SW_MAXIMIZE = 3;
+        private const int SW_MINIMIZE = 6;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        struct APPBARDATA
+        {
+            public int cbSize;
+            public IntPtr hWnd;
+            public int uCallbackMessage;
+            public int uEdge;
+            public RECT rc;
+            public int lParam;
+        }
+        enum ABMsg : int
+        {
+            ABM_NEW = 0,
+            ABM_REMOVE,
+            ABM_QUERYPOS,
+            ABM_SETPOS,
+            ABM_GETSTATE,
+            ABM_GETTASKBARPOS,
+            ABM_ACTIVATE,
+            ABM_GETAUTOHIDEBAR,
+            ABM_SETAUTOHIDEBAR,
+            ABM_WINDOWPOSCHANGED,
+            ABM_SETSTATE
+        }
+        enum ABNotify : int
+        {
+            ABN_STATECHANGE = 0,
+            ABN_POSCHANGED,
+            ABN_FULLSCREENAPP,
+            ABN_WINDOWARRANGE
+        }
+        enum ABEdge : int
+        {
+            ABE_LEFT = 0,
+            ABE_TOP,
+            ABE_RIGHT,
+            ABE_BOTTOM
+        }
+        ////////////////////////////////////////////////////////////
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("SHELL32", CallingConvention = CallingConvention.StdCall)]
+        static extern uint SHAppBarMessage(int dwMessage, ref APPBARDATA pData);
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref RECT Rect);
+        private delegate bool EnumWindowsProc(IntPtr hWnd, int lParam);
+
+        [DllImport("USER32.DLL")]
+        private static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+        [DllImport("USER32.DLL")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("USER32.DLL")]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("USER32.DLL")]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("USER32.DLL")]
+        private static extern IntPtr GetShellWindow();
+        ////////////////////////////////////////////////////////////
+        
+        public static void SetDesktopBackgroundColor()
+        {
+            WallpaperColorChanger.SetColor(backgroundColor);
+        }
+
+        public static void MinimizeAllTrayWindows()
+        {
+            IntPtr lHwnd = FindWindow("Shell_TrayWnd", null);
+            SendMessage(lHwnd, WM_COMMAND, (IntPtr)MIN_ALL, IntPtr.Zero);
+            Thread.Sleep(500);//Should be replaced
+        }
+
+        public static void MaximizeAllTrayWindows()
+        {
+            IntPtr lHwnd = FindWindow("Shell_TrayWnd", null);
+            SendMessage(lHwnd, WM_COMMAND, (IntPtr)MIN_ALL_UNDO, IntPtr.Zero);
+            Thread.Sleep(500);//Should be replaced
+        }
+
+        public static Rectangle GetTaskBar()
+        {
+            APPBARDATA pData = new APPBARDATA();
+            SHAppBarMessage((int)ABMsg.ABM_GETTASKBARPOS,ref pData);
+            return new Rectangle(pData.rc.left,
+                pData.rc.top,
+                pData.rc.right - pData.rc.left,
+                pData.rc.bottom - pData.rc.top);
+        }
+
+        public static Process[] GetTaskbarProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+            List<Process> taskbarp = new List<Process>();
+            foreach (var item in processes)
+            {
+                if (item.MainWindowTitle.Length > 0 && item.ProcessName.ToLower() != "taskmgr")
+                {
+                    taskbarp.Add(item);
+                }
+            }
+            return taskbarp.ToArray();
+        }
+    
+        public static void MaximizeProcess(Process wnd)
+        {
+            ShowWindow(wnd.MainWindowHandle, SW_NORMAL);
+            Thread.Sleep(500);
+        }
+
+        public static Rectangle GetProcessRectangle(Process wnd)
+        {
+            RECT Rect = new RECT();
+            GetWindowRect(wnd.MainWindowHandle, ref Rect);
+            if (Rect.left >= 0 &&
+                Rect.top >= 0 &&
+                Rect.right >= 0 &&
+                Rect.bottom >= 0)
+                return new Rectangle(Rect.left,
+                    Rect.top,
+                    Rect.right - Rect.left,
+                    Rect.bottom - Rect.top);
+            else
+                return new Rectangle(0, 0, 0, 0);
+        }
+
+        public static IDictionary<IntPtr, string> GetOpenWindows()
+        {
+            IntPtr shellWindow = GetShellWindow();
+            Dictionary<IntPtr, string> windows = new Dictionary<IntPtr, string>();
+            EnumWindows(delegate(IntPtr hWnd, int lParam)
+            {
+                if (hWnd == shellWindow) return true;
+                if (!IsWindowVisible(hWnd)) return true;
+
+                int length = GetWindowTextLength(hWnd);
+                if (length == 0) return true;
+
+                StringBuilder builder = new StringBuilder(length);
+                GetWindowText(hWnd, builder, length + 1);
+
+                windows[hWnd] = builder.ToString();
+                return true;
+
+            }, 0);
+            return windows;
+        }
+    }
+}
---
+++ computer-assisted-instruction/Analysis/Desktop/win_8_1/TaskBar.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace computer_assisted_instruction.Analysis.Desktop.win_8_1
+{
+    public class TaskBar
+    {
+        public static DesktopObject[] m_GetRunningAppfromTaskBar(DesktopObject TaskBar)
+        {
+            Bitmap TaskBarBitmap = TaskBar.ObjBitmap;
+            double[,] filterMatrix = JIP_Matrix.RunningAppPattern;
+            int threshold = 20;
+            List<DesktopObject> apps = new List<DesktopObject>();
+            byte[] pixelBuffer = TaskBarBitmap.JIP_ToGray1D();
+            byte[] resultBuffer = new byte[pixelBuffer.Length];
+            int Stride = TaskBarBitmap.Width * 4;
+
+            int filterWidth = filterMatrix.GetLength(1);
+            int filterHeight = filterMatrix.GetLength(0);
+
+            int filterOffsetWidth = (filterWidth) / 2;
+            int filterOffsetHeight = (filterHeight) / 2;
+            int calcOffset = 0;
+            int byteOffset = 0;
+
+            for (int offsetY = filterOffsetHeight; offsetY <
+                TaskBarBitmap.Height - filterOffsetHeight; offsetY++)
+            {
+                int prevoffsetX = 0;
+                for (int offsetX = filterOffsetWidth; offsetX <
+                    TaskBarBitmap.Width - filterOffsetWidth; offsetX++)
+                {
+                    byteOffset = offsetY *
+                                 Stride +
+                                 offsetX * 4;
+                    int percAvg = 0;
+                    for (int filterY = -filterOffsetHeight;
+                        filterY < filterOffsetHeight; filterY++)
+                    {
+                        for (int filterX = -filterOffsetWidth;
+                            filterX < filterOffsetWidth; filterX++)
+                        {
+
+                            calcOffset = byteOffset +
+                                         (filterX * 4) +
+                                         (filterY * Stride);
+
+                            int diff = Math.Abs(pixelBuffer[calcOffset] - ((byte)filterMatrix[filterY + filterOffsetHeight, filterX + filterOffsetWidth]));
+                            if (diff < threshold)
+                                diff = 0;
+                            int perc = ((255 - diff) * 100) / 255;
+                            percAvg += perc;
+                        }
+                    }
+                    percAvg /= filterWidth * filterHeight;
+                    if (percAvg > 65 && Math.Abs(offsetX - prevoffsetX) > (filterWidth / 2))//65
+                    {
+                        percAvg = 100;
+                        DesktopObject dObj = new DesktopObject(TaskBar.Screen);
+                        dObj.ObjPosition =
+                            new Rectangle(new Point(TaskBar.ObjPosition.X + offsetX - (filterWidth / 2),
+                                                    TaskBar.ObjPosition.Y + offsetY),
+                                new Size(filterWidth, TaskBarBitmap.Height)
+                            );
+                        apps.Add(dObj);
+                        prevoffsetX = offsetX;
+                    }
+                }
+            }
+            return apps.ToArray();
+        }
+        public static DesktopObject m_GetTaskBar(Bitmap Screen)
+        {
+            JIP_Line[] lines = Screen.JIP_GetHorizontalLines();
+            JIP_Line taskbarLine = lines[0]; //lines[lines.Length - 1];
+
+            int w = Math.Abs(taskbarLine.B.X - taskbarLine.A.X);
+            int h = Math.Abs(Screen.Height - taskbarLine.A.Y);
+            DesktopObject dObj = new DesktopObject(Screen);
+            dObj.ObjPosition = new Rectangle(taskbarLine.A.X, taskbarLine.A.Y, w, h);
+            return dObj;
+        }
+        
+        public static Color HSL2RGB(double h, double sl, double l)
+        {
+            double v;
+            double r, g, b;
+
+            r = l;   // default to gray
+            g = l;
+            b = l;
+            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
+            if (v > 0)
+            {
+                double m;
+                double sv;
+                int sextant;
+                double fract, vsf, mid1, mid2;
+
+                m = l + l - v;
+                sv = (v - m) / v;
+                h *= 6.0;
+                sextant = (int)h;
+                fract = h - sextant;
+                vsf = v * sv * fract;
+                mid1 = m + vsf;
+                mid2 = v - vsf;
+                switch (sextant)
+                {
+                    case 0:
+                        r = v;
+                        g = mid1;
+                        b = m;
+                        break;
+                    case 1:
+                        r = mid2;
+                        g = v;
+                        b = m;
+                        break;
+                    case 2:
+                        r = m;
+                        g = v;
+                        b = mid1;
+                        break;
+                    case 3:
+                        r = m;
+                        g = mid2;
+                        b = v;
+                        break;
+                    case 4:
+                        r = mid1;
+                        g = m;
+                        b = v;
+                        break;
+                    case 5:
+                        r = v;
+                        g = m;
+                        b = mid2;
+                        break;
+                }
+            }
+            byte R = Convert.ToByte(r * 255.0f);
+            byte G = Convert.ToByte(g * 255.0f);
+            byte B = Convert.ToByte(b * 255.0f);
+            return Color.FromArgb(R,G,B);
+        }
+    }
+}
